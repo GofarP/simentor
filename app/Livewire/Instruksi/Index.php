4dfa56db9a782e181678650c56d55fb35985de26
Gofar Putra <gofarperdana83@gmail.com>
2025-10-01 05:49:08 +0700

    add fill ter jenis pesan

--- app/Livewire/Instruksi/Index.php
+++ app/Livewire/Instruksi/Index.php
@@ -11,6 +11,7 @@ class Index extends Component
     use WithPagination;
 
     public string $search="";
+    public string $jenisPesan;
 
     protected InstruksiServiceInterface $instruksiService;
 
