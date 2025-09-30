11c0cb6bb280f281a4d43b80f2f087cf400f399d
Gofar Putra Perdana <48397661+GofarP@users.noreply.github.com>
2025-09-30 08:05:57 +0700

    Add new item on sidebar

---
+++ app/Http/Controllers/KoordinasiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Koordinasi;
+use Illuminate\Http\Request;
+
+class KoordinasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Koordinasi $koordinasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Koordinasi $koordinasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Koordinasi $koordinasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Koordinasi $koordinasi)
+    {
+        //
+    }
+}
--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -64,11 +64,17 @@
                             href="#0" @click.prevent="open = !open; sidebarExpanded = true">
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center">
-                                    <svg class="shrink-0 fill-current @if (in_array(Request::segment(1), ['koordinasi'])) {{ 'text-violet-500' }}@else{{ 'text-gray-400 dark:text-gray-500' }} @endif"
-                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                        <path
-                                            d="M9 6.855A3.502 3.502 0 0 0 8 0a3.5 3.5 0 0 0-1 6.855v1.656L5.534 9.65a3.5 3.5 0 1 0 1.229 1.578L8 10.267l1.238.962a3.5 3.5 0 1 0 1.229-1.578L9 8.511V6.855ZM6.5 3.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm4.803 8.095c.005-.005.01-.01.013-.016l.012-.016a1.5 1.5 0 1 1-.025.032ZM3.5 11c.474 0 .897.22 1.171.563l.013.016.013.017A1.5 1.5 0 1 1 3.5 11Z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 fill-current" width="16"
+                                        height="16" viewBox="0 0 24 24">
+                                        <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 
+                                                1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 
+                                                2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 
+                                                3zm0 2c-2.33 0-7 1.17-7 
+                                                3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 
+                                                0c-.29 0-.62.02-.97.05 1.16.84 1.97 
+                                                2.06 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" />
                                     </svg>
+
                                     <span
                                         class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Koordinasi</span>
                                 </div>
@@ -89,21 +95,194 @@
                                     <a class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate "
                                         href="#">
                                         <span
-                                            class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Masukan
-                                            Koordinasi</span>
-                                    </a>
-                                </li>
-                                <li class="mb-1 last:mb-0">
-                                    <a class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate "
-                                        href="#">
-                                        <span
-                                            class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Penilaian</span>
+                                            class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Koordinasi</span>
                                     </a>
                                 </li>
 
                             </ul>
                         </div>
                     </li>
+
+                    <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-linear-to-r
+                    @if (in_array(Request::segment(1), ['instruksi']))
+                        from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04]
+                    @endif" x-data="{ open: {{ in_array(Request::segment(1), ['instruksi']) ? 'true' : 'false' }} }">
+
+                        <!-- Parent link -->
+                        <a href="#0" @click.prevent="open = !open; sidebarExpanded = true" class="block text-gray-800 dark:text-gray-100 truncate transition
+                            @if (!in_array(Request::segment(1), ['instruksi']))
+                                hover:text-gray-900 dark:hover:text-white
+                            @endif">
+                            <div class="flex items-center justify-between">
+                                <!-- Icon + label -->
+                                <div class="flex items-center">
+                                    <!-- Ikon pensil -->
+                                    <svg class="shrink-0 stroke-current
+                                        @if (in_array(Request::segment(1), ['instruksi']))
+                                            text-violet-500
+                                        @else
+                                            text-gray-400 dark:text-gray-500
+                                        @endif" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M12 20h9" />
+                                        <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4z" />
+                                    </svg>
+                                    <span
+                                        class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                        Instruksi
+                                    </span>
+                                </div>
+
+                                <!-- Chevron -->
+                                <div
+                                    class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                    <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-500 transition-transform"
+                                        :class="open ? 'rotate-180' : 'rotate-0'" viewBox="0 0 12 12">
+                                        <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </a>
+
+                        <!-- Submenu -->
+                        <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
+                            <ul class="pl-8 mt-1" :class="open ? 'block' : 'hidden'">
+                                <li class="mb-1 last:mb-0">
+                                    <a href="#"
+                                        class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate">
+                                        <span
+                                            class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                            Instruksi
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-linear-to-r
+                    @if (in_array(Request::segment(1), ['respon']))
+                        from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04]
+                    @endif"
+                    x-data="{ open: {{ in_array(Request::segment(1), ['respon']) ? 'true' : 'false' }} }">
+
+                    <!-- Parent link -->
+                    <a href="#0" @click.prevent="open = !open; sidebarExpanded = true"
+                        class="block text-gray-800 dark:text-gray-100 truncate transition
+                        @if (!in_array(Request::segment(1), ['respon']))
+                            hover:text-gray-900 dark:hover:text-white
+                        @endif">
+                        <div class="flex items-center justify-between">
+                            <!-- Icon + label -->
+                            <div class="flex items-center">
+                                <!-- Ikon panah melingkar -->
+                                <svg class="shrink-0 stroke-current
+                                    @if (in_array(Request::segment(1), ['respon']))
+                                        text-violet-500
+                                    @else
+                                        text-gray-400 dark:text-gray-500
+                                    @endif"
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <polyline points="23 4 23 10 17 10"/>
+                                    <polyline points="1 20 1 14 7 14"/>
+                                    <path d="M3.51 9a9 9 0 0 1 14.13-3.36L23 10M1 14l5.36 5.36A9 9 0 0 0 20.49 15"/>
+                                </svg>
+                                <span
+                                    class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                    Respon Tindak Lanjut
+                                </span>
+                            </div>
+
+                            <!-- Chevron -->
+                            <div
+                                class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-500 transition-transform"
+                                    :class="open ? 'rotate-180' : 'rotate-0'" viewBox="0 0 12 12">
+                                    <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </a>
+
+                    <!-- Submenu -->
+                    <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
+                        <ul class="pl-8 mt-1" :class="open ? 'block' : 'hidden'">
+                            <li class="mb-1 last:mb-0">
+                                <a href="#"
+                                    class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate">
+                                    <span
+                                        class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                        Respon Tindak Lanjut
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+
+                    <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-linear-to-r
+                        @if (in_array(Request::segment(1), ['penilaian']))
+                            from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04]
+                        @endif" x-data="{ open: {{ in_array(Request::segment(1), ['penilaian']) ? 'true' : 'false' }} }">
+
+                        <!-- Parent link -->
+                        <a href="#0" @click.prevent="open = !open; sidebarExpanded = true" class="block text-gray-800 dark:text-gray-100 truncate transition
+                            @if (!in_array(Request::segment(1), ['penilaian']))
+                                hover:text-gray-900 dark:hover:text-white
+                            @endif">
+                            <div class="flex items-center justify-between">
+                                <!-- Icon + label -->
+                                <div class="flex items-center">
+                                    <!-- Ikon bintang -->
+                                    <svg class="shrink-0 stroke-current
+                                    @if (in_array(Request::segment(1), ['penilaian']))
+                                        text-violet-500
+                                    @else
+                                        text-gray-400 dark:text-gray-500
+                                    @endif" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 
+                                     18.18 21.02 12 17.77 5.82 21.02 
+                                     7 14.14 2 9.27 8.91 8.26 12 2" />
+                                    </svg>
+                                    <span
+                                        class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                        Penilaian
+                                    </span>
+                                </div>
+
+                                <!-- Chevron -->
+                                <div
+                                    class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                    <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-500 transition-transform"
+                                        :class="open ? 'rotate-180' : 'rotate-0'" viewBox="0 0 12 12">
+                                        <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </a>
+
+                        <!-- Submenu -->
+                        <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
+                            <ul class="pl-8 mt-1" :class="open ? 'block' : 'hidden'">
+                                <li class="mb-1 last:mb-0">
+                                    <a href="#"
+                                        class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate">
+                                        <span
+                                            class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                            Penilaian
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
 
                     <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-linear-to-r
                     @if (in_array(Request::segment(1), ['role', 'permission'])) from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04] @endif"
@@ -179,7 +358,7 @@
                                         <span class="text-sm font-medium">User</span>
                                     </a>
                                 </li>
-                               
+
                             </ul>
                         </div>
                     </li>
