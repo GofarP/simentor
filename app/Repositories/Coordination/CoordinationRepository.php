7e8626f0a25a47cf83b8d54007ca2adb454fe553
Gofar Putra <gofarperdana83@gmail.com>
2025-10-02 06:11:06 +0700

    add coordination repository

---
+++ app/Repositories/Coordination/CoordinationRepository.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Repositories\Coordination;
+use App\Enums\InstructionType;
+use App\Models\Coordination;
+use App\Models\ForwardInstruction;
+use App\Models\Instruction;
+use App\Repositories\Instruction\CoordinationRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class CoordinationRepository implements CoordinationRepositoryInterface{
+    public function getAll(?string $search = null, int $perPage = 10, InstructionType $coordinationType, bool $eager = false)
+    {
+
+    }
+
+    public function storeCoordination(array $data)
+    {
+
+    }
+
+
+    public function editCoordination(Coordination $coordination, array $data)
+    {
+
+    }
+
+    public function deleteCoordination(Coordination $coordination): bool
+    {
+        return false;
+    }
+
+    public function forwarCoordination(Coordination $coordination, array $data)
+    {
+
+    }
+}
