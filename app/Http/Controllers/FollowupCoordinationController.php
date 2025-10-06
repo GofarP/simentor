821071ad9722e91ce2ed0315efed0444057605ee
Gofar Putra Perdana <48397661+GofarP@users.noreply.github.com>
2025-10-06 13:49:42 +0700

    Add followup coordination

---
+++ app/Http/Controllers/FollowupCoordinationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FollowupCoordination;
+use Illuminate\Http\Request;
+
+class FollowupCoordinationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FollowupCoordination $followupCoordination)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FollowupCoordination $followupCoordination)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FollowupCoordination $followupCoordination)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FollowupCoordination $followupCoordination)
+    {
+        //
+    }
+}
