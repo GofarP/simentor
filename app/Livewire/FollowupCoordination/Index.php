31d5dabfe65f516e11b73e989a05d19f5cb5d25d
GofarP <gofarperdana83@gmail.com>
2025-10-07 10:55:30 +0700

    Tambah Followup coordination

--- app/Livewire/FollowupCoordination/Index.php
+++ app/Livewire/FollowupCoordination/Index.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire\FollowupCoordination;
 
+use App\Enums\MessageType;
 use Livewire\Component;
+use Livewire\WithPagination;
+
+use App\Services\FollowupCoordination\FollowupCoordinationServiceInterface;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    protected string $search = "";
+    public string $messageType = "received";
+
+    protected FollowupCoordinationServiceInterface $followupInstructionService;
+
+    public function boot(FollowupCoordinationServiceInterface $followupInstructionService){
+        $this->followupInstructionService = $followupInstructionService;
+    }
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.followup-coordination.index');
+        $messageEnum=MessageType::from($this->messageType);
+        $followupCoordination=$this->followupInstructionService->getAll($this->search, $messageEnum, 10);
+        return view('livewire.followup-coordination.index',[
+            'followupcoordinations'=>$followupCoordination
+        ]);
     }
 }
