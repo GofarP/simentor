60b1acef08bb1217723c0aac9fcf06623f2f4df0
GofarP <gofarperdana83@gmail.com>
2025-10-07 10:04:28 +0700

    remove error any on followupinstruction

--- resources/views/followupinstruction/create.blade.php
+++ resources/views/followupinstruction/create.blade.php
@@ -6,15 +6,7 @@
 
             <form method="POST" action="{{ route('followupinstruction.store') }}" enctype="multipart/form-data">
                 @csrf
-                @if ($errors->any())
-                    <div class="bg-red-100 text-red-600 p-2 rounded mb-4">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+               
                 <div class="mb-4">
                     <label for="instruction_id"
                         class="block text-gray-700 dark:text-gray-200 font-medium mb-2">Instruksi</label>
