8bf22a80200bf215850f85ac2f369b854266bbe0
Gofar Putra <gofarperdana83@gmail.com>
2025-10-08 05:56:39 +0700

    Add Instruction Score Livewire

--- app/Livewire/InstructionScore/Index.php
+++ app/Livewire/InstructionScore/Index.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\InstructionScore;
 
+use App\Services\Instruction\InstructionService;
+use App\Services\Instruction\InstructionServiceInterface;
 use Livewire\Component;
-
+use Livewire\WithPagination;
+use App\Services\InstructionScore\InstructionScoreServiceInterface;
 class Index extends Component
 {
+    use WithPagination;
+
+    public string $search="";
+
+
+    protected InstructionScoreServiceInterface $instructionScoreService;
+
+    public function boot(InstructionScoreServiceInterface $instructionScoreService){
+        $this->instructionScoreService=$instructionScoreService;
+    }
+
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.instruction-score.index');
+        $instructionScores=$this->instructionScoreService->getAllInstructionScore($this->search, 10);
+        return view('livewire.instruction-score.index',[
+            'instructionScores'=>$instructionScores
+        ]);
     }
 }
