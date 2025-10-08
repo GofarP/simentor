18904efdb115e48c2173d2736f6427022043630f
GofarP <gofarperdana83@gmail.com>
2025-10-08 08:13:38 +0700

    Fix select for getting the current instruction

--- resources/views/instructionscore/edit.blade.php
+++ resources/views/instructionscore/edit.blade.php
@@ -5,20 +5,19 @@
 
             <form method="POST" action="{{ route('instructionscore.update', $instructionscore->id) }}">
                 @csrf
-
+                @method('PUT')
+                
                 {{-- Penerima --}}
                 <div class="mb-4">
                     <label for="instruction_id" class="block text-gray-700 dark:text-gray-200 font-medium mb-2">
                         Instruksi
                     </label>
-                    <select name="instruction_id" id="receiver_id" class="form-control js-example-basic-single w-full">
-
+                    <select name="instruction_id" id="instruction_id" class="form-control js-example-basic-single w-full">
                     </select>
                     @error('instruction_id')
-                        <span class="text-red-600 text-sm mt-1 block">{{ $message }}</span>
+                    <span class="text-red-600 text-sm mt-1 block">{{ $message }}</span>
                     @enderror
                 </div>
-
                 <div class="mb-4">
                     <label for="score" class="block text-gray-700 dark:text-gray-200 font-medium mb-2">
                         Penilaian
@@ -29,7 +28,7 @@
                         <option value="0" {{ $instructionscore->score === 0 ? 'selected' : '' }}>Buruk</option>
                     </select>
                     @error('score')
-                        <span class="text-red-600 text-sm mt-1 block">{{ $message }}</span>
+                    <span class="text-red-600 text-sm mt-1 block">{{ $message }}</span>
                     @enderror
                 </div>
 
@@ -50,52 +49,53 @@
     </div>
 
     @push('css')
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="https://unpkg.com/trix@2.1.0/dist/trix.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/trix@2.1.0/dist/trix.css">
     @endpush
 
     @push('js')
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://unpkg.com/trix@2.1.0/dist/trix.umd.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        <script>
-            $(document).ready(function() {
-                const $instructionSelect = $('#instruction_id');
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/trix@2.1.0/dist/trix.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            const $instructionSelect = $('#instruction_id');
 
-                $instructionSelect.select2({
-                    placeholder: 'Cari instruksi...',
-                    ajax: {
-                        url: '{{ route('instructions.search') }}',
-                        dataType: 'json',
-                        delay: 250,
-                        data: function(params) {
-                            return {
-                                search: params.term
-                            };
-                        },
-                        processResults: function(data) {
-                            return {
-                                results: data.results.map(item => ({
-                                    id: item.id,
-                                    text: item.title
-                                }))
-                            };
-                        },
-                        cache: true
+            $instructionSelect.select2({
+                placeholder: 'Cari instruksi...',
+                ajax: {
+                    url: '{{ route('instructions.search') }}',
+                    dataType: 'json',
+                    delay: 250,
+                    data: function(params) {
+                        return {
+                            search: params.term
+                        };
                     },
-                    minimumInputLength: 1,
-                    width: '100%'
-                });
-
-                // Set nilai lama untuk edit
-                const oldId = "{{ old('instruction_id', $followupinstruction->instruction_id) }}";
-                const oldText = "{{ old('instruction_title', $instructionTitle) }}";
-
-                if (oldId) {
-                    const option = new Option(oldText, oldId, true, true);
-                    $instructionSelect.append(option).trigger('change');
-                }
+                    processResults: function(data) {
+                        return {
+                            results: data.results.map(item => ({
+                                id: item.id,
+                                text: item.title
+                            }))
+                        };
+                    },
+                    cache: true
+                },
+                minimumInputLength: 1,
+                width: '100%'
             });
-        </script>
+
+            // Set nilai lama untuk edit
+            const oldId = "{{ old('instruction_id', $instructionscore->instruction_id) }}";
+            const oldText = "{{ old('instruction_title', $instructionTitle) }}";
+
+            if (oldId) {
+                const option = new Option(oldText, oldId, true, true);
+                $instructionSelect.append(option).trigger('change');
+            }
+        });
+    </script>
+
     @endpush
 </x-app-layout>
