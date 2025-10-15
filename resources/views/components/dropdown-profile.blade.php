e2c16c2d85a97e0a2d64daac6adc0f76e8ee219d
GofarP <gofarperdana83@gmail.com>
2025-10-15 16:44:31 +0700

    remove settings

--- resources/views/components/dropdown-profile.blade.php
+++ resources/views/components/dropdown-profile.blade.php
@@ -35,9 +35,7 @@
             <div class="text-xs text-gray-500 dark:text-gray-400 italic">{{ Auth::user()->getRoleNames()->first() }}</div>
         </div>
         <ul>
-            <li>
-                <a class="font-medium text-sm text-violet-500 hover:text-violet-600 dark:hover:text-violet-400 flex items-center py-1 px-3" href="{{ route('profile.show') }}" @click="open = false" @focus="open = true" @focusout="open = false">Settings</a>
-            </li>
+           
             <li>
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
