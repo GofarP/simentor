5579b4ce5654dddb99a5494002a704a9fdf9a75e
GofarP <gofarperdana83@gmail.com>
2025-10-15 10:39:25 +0700

    Fix followupinstruction score

--- app/Livewire/FollowupInstructionScore/Index.php
+++ app/Livewire/FollowupInstructionScore/Index.php
@@ -7,7 +7,6 @@ use App\Models\Instruction;
 use Livewire\WithPagination;
 use App\Models\FollowupInstruction;
 use Illuminate\Support\Facades\Auth;
-use App\Models\FollowupInstructionScore;
 
 class Index extends Component
 {
